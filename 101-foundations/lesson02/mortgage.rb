fb4cc9f91c92e49d5440b90c67e8ee0ce665a23c
kennyfrc <fxkennyfrc@gmail.com>
2016-02-03 16:25:37 +0800

    refactored mortgage calculator

--- 101-foundations/lesson02/mortgage.rb
+++ 101-foundations/lesson02/mortgage.rb
@@ -5,8 +5,8 @@ def prompt(message)
 end
 
 def valid_number?(num)
-  num_in_cents = (num.to_f * 100).round.to_i
-  num_in_cents != 0
+  num_in_cents = (num.to_f * 100).round # no need to call to_i because round will return a Fixnum
+  num_in_cents > 0
 end
 
 def valid_number_of_months?(num)
@@ -21,7 +21,7 @@ prompt("Hello, #{name}! Nice to meet you.")
 
 loop do
   welcome_message = <<-MSG
-  \nTo calculate the monthly payment & loan balance,
+  \nTo calculate the monthly payment,
   we will need the following information from you:
     1) Loan Amount Desired
     2) Quoted Annual Percentage Rate
@@ -45,39 +45,35 @@ loop do
   end
 
   loop do
+    prompt("Input 2 of 3: What is the quoted annual percentage rate?")
+
+    apr = gets.chomp
+
+    break if valid_number?(apr)
+    prompt("Please use valid numbers only. '0' is also not allowed.")
+    sleep 1
+  end
+
+  if apr.to_f < 1
     loop do
-      prompt("Input 2 of 3: What is the quoted annual percentage rate?")
+      prompt("Did you mean:\nOption 1: #{apr.to_f * 100}% ?\n- or - \nOption 2: #{apr.to_f}% ?\nEnter '1' or '2' to select.")
+      apr_answer = gets.chomp
 
-      apr = gets.chomp
-
-      break if valid_number?(apr)
-      prompt("Please use valid numbers only. '0' is also not allowed.")
-      sleep 1
-    end
-
-    if apr.to_f < 1
-      loop do
-        prompt("Did you mean:\nOption 1: #{apr.to_f * 100}% ?\n- or - \nOption 2: #{apr.to_f}% ?\nEnter '1' or '2' to select.")
-        apr_answer = gets.chomp
-
-        case apr_answer
-        when '1'
-          prompt("OK. Using #{apr.to_f * 100}%...") if apr_answer == '1'
-          apr = (apr.to_f * 100).to_s
-          break
-        when '2'
-          prompt("OK. Using #{apr.to_f}%...") if apr_answer == '2'
-          break
-        else
-          prompt("Please use valid numbers only.")
-          sleep 1
-          prompt("Resetting...")
-          sleep 1
-        end
+      case apr_answer
+      when '1'
+        prompt("OK. Using #{apr.to_f * 100}%...") if apr_answer == '1'
+        apr = (apr.to_f * 100).to_s
+        break
+      when '2'
+        prompt("OK. Using #{apr.to_f}%...") if apr_answer == '2'
+        break
+      else
+        prompt("Please use valid numbers only.")
+        sleep 1
+        prompt("Resetting...")
+        sleep 1
       end
     end
-
-    break
   end
 
   loop do
@@ -97,23 +93,21 @@ loop do
   prompt("Crunching some numbers...")
   sleep 1
 
-  fixed_monthly_payment = loan_amount * (apr_in_months * (1 + apr_in_months)**loan_duration) / (((1 + apr_in_months)**loan_duration) - 1)
-  loan_balance = loan_amount * ((((1 + apr_in_months)**loan_duration) - ((1 + apr_in_months)**loan_duration))) / (((1 + apr_in_months)**loan_duration) - 1)
+  fixed_monthly_payment = loan_amount * 
+                          (apr_in_months * (1 + apr_in_months)**loan_duration) / 
+                          (((1 + apr_in_months)**loan_duration) - 1)
 
   prompt("Your fixed monthly payment is #{format('%02.2f', fixed_monthly_payment)} USD.")
   sleep 1
-  prompt("Your loan balance is #{loan_balance.round(2)}.")
-  sleep 1
 
-  prompt("Would you like to make another calculation? (Y to continue / N to exit)")
+  prompt("Would you like to make another calculation? (Y to continue / N to exit / Exits for any other input)")
 
   ans = gets.chomp
 
-  case ans.downcase
-  when 'n'
+  if ans.downcase == 'n' || ans.downcase != 'y'
     prompt("Bye!")
     break
-  when 'y'
+  else
     prompt("OK.. Resetting...")
     sleep 1
   end
