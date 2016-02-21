ab0befbe89847f9bbbd9446a4d161019d2246ed7
kennyfrc <fxkennyfrc@gmail.com>
2016-02-21 20:20:08 +0800

    refactored

--- 101-foundations/lesson02/rpssl.rb
+++ 101-foundations/lesson02/rpssl.rb
@@ -1,109 +1,73 @@
 VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
 
+WIN_RULES = { 'lizard' => { win_conditions: %w(paper rock)},
+              'rock' => { win_conditions: %w(lizard scissors)},
+              'paper' => { win_conditions: %w(rock spock)},
+              'scissors' => { win_conditions: %w(paper lizard)},
+              'spock' => { win_conditions: %w(rock scissors)}
+            }
+
 def prompt(msg)
   Kernel.puts("=> #{msg}")
 end
 
 # win conditions for the game logic
 
-def rock_win_conditions(player, computer)
-  (player == 'rock' && computer == 'scissors') ||
-  (player == 'rock' && computer == 'lizard')
-end
-
-def paper_win_conditions(player, computer)
-  (player == 'paper' && computer == 'spock') ||
-  (player == 'paper' && computer == 'rock')
-end
-
-def scissors_win_conditions(player, computer)
-  (player == 'scissors' && computer == 'lizard') ||
-  (player == 'scissors' && computer == 'paper')
-end
-
-def lizard_win_conditions(player, computer)
-  (player == 'lizard' && computer == 'paper') || 
-  (player == 'lizard' && computer == 'spock')
-end
-
-def spock_win_conditions(player, computer)
-  (player == 'spock' && computer == 'scissors') ||
-  (player == 'spock' && computer == 'rock')
-end
-
-# loss conditions for the game logic
-
-def rock_loss_conditions(player, computer)
-  (player == 'rock' && computer == 'paper') ||
-  (player == 'rock' && computer == 'spock')
-end
-
-def paper_loss_conditions(player, computer)
-  (player == 'paper' && computer == 'lizard') ||
-  (player == 'paper' && computer == 'scissors')
-end
-
-def scissors_loss_conditions(player, computer)
-  (player == 'scissors' && computer == 'spock') ||
-  (player == 'scissors' && computer == 'rock')
-end
-
-def lizard_loss_conditions(player, computer)
-  (player == 'lizard' && computer == 'rock') || 
-  (player == 'lizard' && computer == 'scissors')
-end
-
-def spock_loss_conditions(player, computer)
-  (player == 'spock' && computer == 'paper') ||
-  (player == 'spock' && computer == 'lizard')
+def win_round?(player_choice, computer_choice)
+  WIN_RULES.any? do |choice, player_wins_if|
+    choice == player_choice && player_wins_if[:win_conditions].include?(computer_choice)
+  end
 end
 
 # win / loss logger
 
-def log_win
-  $wl_log_player << 1
-  $wl_log_computer << 0
-end
-
-def log_loss
-  $wl_log_player << 0
-  $wl_log_computer << 1
+def log_results!(player, computer, player_log, computer_log)
+  if win_round?(player, computer)
+    player_log << 1
+    computer_log << 0
+    return player_log, computer_log
+  elsif player == computer
+    player_log << 0
+    computer_log << 0
+    return player_log, computer_log
+  else
+    player_log << 0
+    computer_log << 1
+    return player_log, computer_log
+  end
 end
 
 # actual game logic
 
 def display_results(player, computer)
-  if  rock_win_conditions(player, computer) ||
-      paper_win_conditions(player, computer) ||
-      scissors_win_conditions(player, computer) ||
-      lizard_win_conditions(player, computer) ||
-      spock_win_conditions(player, computer)
+  if win_round?(player, computer)
     prompt("1 Point for Player!")
-    log_win
-  elsif rock_loss_conditions(player, computer) ||
-        paper_loss_conditions(player, computer) ||
-        scissors_loss_conditions(player, computer) ||
-        lizard_loss_conditions(player, computer) ||
-        spock_loss_conditions(player, computer)
-    prompt("1 Point for Computer!")
-    log_loss
-  else
+  elsif player == computer
     prompt("It's a tie!")
+  else
+    prompt("1 Point for Computer!")
   end
 end
 sleep 1
+
+# round counter
+
+def count_this_round!(round)
+  round << 1
+end
 
 # user-facing code is below this line
 
 prompt("Welcome to the RPSSL Tournament! Whoever gets 5 points first wins! Good Luck!")
 sleep 1
 
-$wl_log_player = []
-$wl_log_computer = []
+player_log = []
+computer_log = []
 rounds = [1]
 
 loop do
   choice = ''
+
   loop do
     prompt("ROUND #{rounds.reduce(:+)}")
     sleep 1
@@ -111,17 +75,15 @@ loop do
     choice = Kernel.gets().chomp().downcase
     sleep 1
 
-    case choice
-    when 'r'
-      choice = 'rock'
-    when 'p'
-      choice = 'paper'
-    when 'sc'
-      choice = 'scissors'
-    when 'l'
-      choice = 'lizard'
-    when 'sp'
-      choice = 'spock'
+    choice =
+      case choice
+      when 'r' then 'rock'
+      when 'p' then 'paper'
+      when 'sc' then 'scissors'
+      when 'l' then 'lizard'
+      when 'sp' then 'spock'
+      else
+        choice
     end
 
     break if VALID_CHOICES.include?(choice)
@@ -135,36 +97,38 @@ loop do
   sleep 1
 
   display_results(choice, computer_choice)
-  rounds << 1
+  log_results!(choice, computer_choice, player_log, computer_log)
+  count_this_round!(rounds)
+  
   sleep 1
 
   puts "-------------"
-  puts "/SCOREBOARD/\nPLAYER: #{$wl_log_player.count(1)}\nCOMPUTER: #{$wl_log_computer.count(1)}"
+  puts "/SCOREBOARD/\nPLAYER: #{player_log.count(1)}\nCOMPUTER: #{computer_log.count(1)}"
   puts "-------------"
   sleep 1
 
-  if [$wl_log_computer.reduce(:+), $wl_log_player.reduce(:+)].include?(5)
-    if $wl_log_computer.reduce(:+) == 5
+  if [computer_log.reduce(:+), player_log.reduce(:+)].include?(5)
+    if computer_log.reduce(:+) == 5
       prompt("The Computer has won the game! Better luck next time")
-      sleep 2
+      sleep 1
 
       prompt("Do you want to play more? (Y for yes / N for no)")
       ans = gets.chomp.downcase
       break if ans == 'n'
 
-      $wl_log_player = []
-      $wl_log_computer = []
+      player_log = []
+      computer_log = []
       rounds = [1]
     else
       prompt("You have won! Woohoo!")
-      sleep 2
+      sleep 1
       
       prompt("Do you want to play more? (Y for yes / N for no)")
       ans = gets.chomp.downcase
       break if ans == 'n'
 
-      $wl_log_player = []
-      $wl_log_computer = []
+      player_log = []
+      computer_log = []
       rounds = [1]
     end
   end
